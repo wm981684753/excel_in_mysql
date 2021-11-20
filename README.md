d0b5eba93cea09d6051731217fe314b20b52e869
Weiming <Weiming.Wei@feisu.com>
2021-11-20 18:06:13 +0800

    excel字段过多，拆分表优化

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # excel_in_mysql
 
 基于Python3 一键导入excel到mysql的脚本
+
 只需预先创建一个空数据库
 数据表自动生成（字段过多会做拆分表处理）
 在config.ini中修改数据库配置
@@ -8,3 +9,20 @@
 excel.py为运行主程序
 
 tip：下载项目后需要拉取公共依赖包
+
+配置文件说明config.ini
+
+host：数据库地址
+
+database：数据库名称
+
+username：用户名
+
+password：密码
+
+fieldlen：单个字段长度（默认类型为varchar）
+
+maxfield：单表最大字段数（建议不超过50）
+
+
+
--- excel.py
+++ excel.py
@@ -1,6 +1,8 @@
 # coding:utf-8
 # 基于python2的 简单的单表excel入库脚本
 # author weiming 2021.11
+import math
+
 import xlrd
 import pymysql
 import time
@@ -13,10 +15,10 @@ database = config.database  # 数据库名称
 username = config.username  # 数据库用户名
 password = config.password  # 数据库密码
 
-# ------参数定义
+# ------输入参数
 # 需要导入的文件地址
-file = "demo.xls"
-# file = input('请输入你要导入的excel文件：')
+# file = "demo.xls"
+file = input('请输入你要导入的excel文件：')
 
 # 文件校验
 ext = os.path.splitext(file)[1]
@@ -57,41 +59,58 @@ for x in range(26):
             excelAlpTitle.append(alpArr[x] + alpArr[y])
 
 # 根据字段长度，需要做拆分表处理
-for index in range(len(excelTitle)):
-    print(1)
-
-# 先判断数据库是否有表，若无当前表，则自动创建
-drop = "DROP TABLE IF EXISTS `" + table + "`;"
-cur.execute(drop)
-db.commit()
-create = "CREATE TABLE `" + table + "` ("
-create += "`id` int(11) NOT NULL AUTO_INCREMENT,"
-for index, item in enumerate(excelTitle):
-    create += "`" + excelAlpTitle[index] + "` varchar(330) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '" + item + "',"
-create += "PRIMARY KEY (`id`)"
-create += ") ENGINE=MyISAM AUTO_INCREMENT=70001 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;"
-cur.execute(create)
-db.commit()
-
-# 跳过第一行的标题，直接从第二行真实数据开始
-count = 0
-for row_num in range(1, rows):
-    # 循环取出每一列的值并写入数据表
-    fields = values = ""
-    for index, item in enumerate(excelTitle):
-        data = db.escape_string(sheet.row_values(row_num, 0)[index])
-        fields = fields + "," + excelAlpTitle[index]
-        values = values + ',"' + data + '"'
-    # 截取字符串，丢弃第一个字符
-    fields = fields[1:]
-    values = values[1:]
-    sql = "insert into " + table + "(" + fields + ") VALUES (" + values + ")"
-    cur.execute(sql)
+maxFieldNum = int(config.maxfield)  # 单表最大字段数
+tableNum = math.ceil(len(excelTitle) / maxFieldNum)  # 需要拆分成的表的数量
+tableStr = ""  # 本次写入的表
+dataCount = 0  # 插入的总数据量
+for num in range(0, tableNum):
+    # 先判断数据库是否有表，若无当前表，则自动创建
+    if (num == 0):
+        tableName = table
+    else:
+        tableName = table + str(num + 1)
+    if (len(tableStr) > 0):
+        tableStr = tableStr + "," + tableName
+    else:
+        tableStr = tableName
+    drop = "DROP TABLE IF EXISTS `" + tableName + "`;"
+    cur.execute(drop)
     db.commit()
-    count = count + 1
+    create = "CREATE TABLE `" + tableName + "` ("
+    create += "`id` int(11) NOT NULL AUTO_INCREMENT,"
+    for index, item in enumerate(excelTitle[num * maxFieldNum:(num + 1) * maxFieldNum]):
+        index = index + num * maxFieldNum
+        create += "`" + excelAlpTitle[
+            index] + "` varchar("+config.fieldlen+") COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '" + item + "',"
+    create += "PRIMARY KEY (`id`)"
+    create += ") ENGINE=MyISAM AUTO_INCREMENT=70001 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;"
+    cur.execute(create)
+    cur.execute("truncate table " + tableName)
+    db.commit()
+
+    # 跳过第一行的标题，直接从第二行真实数据开始
+    for row_num in range(1, rows):
+        # 循环取出每一列的值并写入数据表
+        fields = values = ""
+        for index, item in enumerate(excelTitle[num * maxFieldNum:(num + 1) * maxFieldNum]):
+            index = index + num * maxFieldNum
+            data = db.escape_string(sheet.row_values(row_num, 0)[index])
+            fields = fields + "," + excelAlpTitle[index]
+            values = values + ',"' + data + '"'
+        # 截取字符串，丢弃第一个字符
+        fields = fields[1:]
+        values = values[1:]
+        sql = "insert into " + tableName + "(" + fields + ") VALUES (" + values + ")"
+        cur.execute(sql)
+        db.commit()
+        dataCount = dataCount + 1
+
 cur.close()
 db.close()
 t2 = time.time()
 print("一共用时：" + str(t2 - t1) + " 秒")
-print("插入数据：" + str(count) + " 条")
+print("共写入 " + str(tableNum) + " 张表：" + tableStr)
+print("单表字段：" + str(maxFieldNum) + " 总字段：" + str(len(excelTitle)))
+print("单表写入：" + str(int(dataCount / tableNum)) + " 条")
+print("总共写入：" + str(dataCount) + " 条")
 print("入库完成")
